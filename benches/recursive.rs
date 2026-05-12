fd566accc75df35995520da716ff67f9550e021c
mentalblood0 <necheporenko.stepan@yandex.ru>
2026-05-12 16:16:23 +0300

    parametrize benchmark

--- benches/recursive.rs
+++ benches/recursive.rs
@@ -4,58 +4,58 @@ use serde_json::json;
 
 fn fibonacci_benchmark(bencher_context: &mut Criterion) {
     let interpreter = Interpreter::default();
-    let program = serde_json::from_value::<ValueWithIncludes>(json!({
-        "WITH": {
-            "DEFINITIONS": {
-                "FIBONACCI": {
-                    "IF": {
-                        "IS_SORTED": [
-                            "_",
-                            1
-                        ]
-                    },
-                    "THEN": "_",
-                    "ELSE": {
-                        "WITH": {
-                            "CONSTANTS": {
-                                "x": "_"
-                            }
-                        },
-                        "COMPUTE": {
-                            "SUM": [
-                                {
-                                    "FIBONACCI": {
-                                        "SUM": [
-                                            "x",
-                                            -1
-                                        ]
-                                    }
-                                },
-                                {
-                                    "FIBONACCI": {
-                                        "SUM": [
-                                            "x",
-                                            -2
-                                        ]
-                                    }
-                                }
+    let mut includes_cache = IncludesCache::default();
+    for number in [22, 23, 24] {
+        let program = serde_json::from_value::<ValueWithIncludes>(json!({
+            "WITH": {
+                "DEFINITIONS": {
+                    "FIBONACCI": {
+                        "IF": {
+                            "IS_SORTED": [
+                                "_",
+                                1
                             ]
+                        },
+                        "THEN": "_",
+                        "ELSE": {
+                            "WITH": {
+                                "CONSTANTS": {
+                                    "x": "_"
+                                }
+                            },
+                            "COMPUTE": {
+                                "SUM": [
+                                    {
+                                        "FIBONACCI": {
+                                            "SUM": [
+                                                "x",
+                                                -1
+                                            ]
+                                        }
+                                    },
+                                    {
+                                        "FIBONACCI": {
+                                            "SUM": [
+                                                "x",
+                                                -2
+                                            ]
+                                        }
+                                    }
+                                ]
+                            }
                         }
                     }
                 }
+            },
+            "COMPUTE": {
+                "FIBONACCI": number
             }
-        },
-        "COMPUTE": {
-            "FIBONACCI": 22
-        }
-    }))
-    .unwrap();
-
-    let mut includes_cache = IncludesCache::default();
-
-    bencher_context.bench_function("fibonacci_recursive_10", |b| {
-        b.iter(|| interpreter.compute(&program, &mut includes_cache).unwrap())
-    });
+        }))
+        .unwrap();
+        bencher_context.bench_function(&format!("fibonacci_recursive_{number}"), |b| {
+            b.iter(|| interpreter.compute(&program, &mut includes_cache).unwrap())
+        });
+    }
 }
 
 criterion_group!(benches, fibonacci_benchmark);
