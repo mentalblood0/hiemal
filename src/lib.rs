ed28f68566130220e9d8316919eb4d61bbf29474
mentalblood0 <necheporenko.stepan@yandex.ru>
2026-06-11 17:38:35 +0300

    complex recursion test passes

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ mod tests {
                                 ]
                               }
                             },
-                            1,
+                            0,
                             {
                               "fibonacci:": {
                                 "sum": [
@@ -74,6 +74,120 @@ mod tests {
                 },
                 "compute": {
                   "fibonacci:": 10
+                }
+            }}))
+            .unwrap(),
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_recursive_big() {
+        compile(
+            &serde_json::from_value(json!({
+              "scope": {
+                "functions": {
+                  "fibonacci_1:": {
+                    "branching": {
+                      "if": {
+                        "is sorted": [
+                          "_",
+                          1
+                        ]
+                      },
+                      "then": "_",
+                      "else": {
+                          "sum": [
+                            {
+                              "fibonacci_2:": {
+                                "sum": [
+                                  "_",
+                                  -1
+                                ]
+                              }
+                            },
+                            0,
+                            {
+                              "fibonacci_3:": {
+                                "sum": [
+                                  "_",
+                                  -2
+                                ]
+                              }
+                            }
+                          ]
+                      }
+                    }
+                  },
+                  "fibonacci_2:": {
+                    "branching": {
+                      "if": {
+                        "is sorted": [
+                          "_",
+                          1
+                        ]
+                      },
+                      "then": "_",
+                      "else": {
+                          "sum": [
+                            {
+                              "fibonacci_1:": {
+                                "sum": [
+                                  "_",
+                                  -1
+                                ]
+                              }
+                            },
+                            0,
+                            {
+                              "fibonacci_3:": {
+                                "sum": [
+                                  "_",
+                                  -2
+                                ]
+                              }
+                            }
+                          ]
+                      }
+                    }
+                  },
+                  "fibonacci_3:": {
+                    "branching": {
+                      "if": {
+                        "is sorted": [
+                          "_",
+                          1
+                        ]
+                      },
+                      "then": "_",
+                      "else": {
+                          "sum": [
+                            {
+                              "fibonacci_1:": {
+                                "sum": [
+                                  "_",
+                                  -1
+                                ]
+                              }
+                            },
+                            0,
+                            {
+                              "fibonacci_2:": {
+                                "sum": [
+                                  "_",
+                                  -2
+                                ]
+                              }
+                            }
+                          ]
+                      }
+                    }
+                  }
+                },
+                "compute": {
+                  "fibonacci_1:": 10,
+                  "fibonacci_2:": 10,
+                  "fibonacci_3:": 10,
                 }
             }}))
             .unwrap(),
