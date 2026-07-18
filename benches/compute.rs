c60648b77103a543036d04f5bdfe175e12aab7a5
mentalblood0 <necheporenko.stepan@yandex.ru>
2026-07-18 19:20:24 +0300

    benchmarks for std and rpds vectors

--- benches/compute.rs
+++ benches/compute.rs
@@ -1,21 +1,24 @@
+use std::sync::Arc;
+
 use criterion::{Criterion, criterion_group, criterion_main};
 use hiemal::{
     compiler::Compiler,
     computer::{Computer, ComputerConfig},
     program::Program,
 };
+use nanorand::{Rng, WyRand};
 
 fn benchmarks(bencher_context: &mut Criterion) {
-    let computer = Computer::default();
-    let compiler = Compiler {
-        metaprograms_computer: computer.clone(),
-    };
-    let computer_with_caching = Computer {
-        config: ComputerConfig {
-            user_functions_caching: true,
-        },
-    };
     {
+        let computer = Computer::default();
+        let compiler = Compiler {
+            metaprograms_computer: computer.clone(),
+        };
+        let computer_with_caching = Computer {
+            config: ComputerConfig {
+                user_functions_caching: true,
+            },
+        };
         for number in [22, 23, 24] {
             let program = serde_saphyr::from_str::<Program>(
                 &r#"functions:
@@ -48,6 +51,90 @@ compute:
                 })
             });
         }
+    }
+    {
+        static SEED: u64 = 0;
+        static VECTOR_SIZE: usize = 1000;
+        static OPERATIONS_COUNT: usize = 10000;
+        bencher_context.bench_function("set vector std", |b| {
+            b.iter(|| {
+                let mut rng = WyRand::new_seed(SEED);
+                let mut results: Vec<Vec<Arc<usize>>> = Vec::with_capacity(OPERATIONS_COUNT);
+                results.push(Vec::from_iter(
+                    (0..VECTOR_SIZE).map(|_| Arc::new(rng.generate_range(0..usize::MAX))),
+                ));
+                for operation_index in 0..OPERATIONS_COUNT {
+                    let mut new_vector =
+                        results[rng.generate_range(0..operation_index + 1)].clone();
+                    new_vector[rng.generate_range(0..VECTOR_SIZE)] =
+                        Arc::new(rng.generate_range(0..usize::MAX));
+                    results.push(new_vector);
+                }
+            })
+        });
+        bencher_context.bench_function("set vector rpds", |b| {
+            b.iter(|| {
+                let mut rng = WyRand::new_seed(SEED);
+                let mut results: Vec<rpds::VectorSync<usize>> =
+                    Vec::with_capacity(OPERATIONS_COUNT);
+                results.push(rpds::VectorSync::from_iter(
+                    (0..VECTOR_SIZE).map(|_| rng.generate_range(0..usize::MAX)),
+                ));
+                for operation_index in 0..OPERATIONS_COUNT {
+                    let mut new_vector =
+                        results[rng.generate_range(0..operation_index + 1)].clone();
+                    new_vector.set_mut(
+                        rng.generate_range(0..VECTOR_SIZE),
+                        rng.generate_range(0..usize::MAX),
+                    );
+                    results.push(new_vector);
+                }
+            })
+        });
+        bencher_context.bench_function("insert vector std", |b| {
+            b.iter(|| {
+                let mut rng = WyRand::new_seed(SEED);
+                let mut results: Vec<Vec<Arc<usize>>> = Vec::with_capacity(OPERATIONS_COUNT);
+                results.push(Vec::from_iter(
+                    (0..VECTOR_SIZE).map(|_| Arc::new(rng.generate_range(0..usize::MAX))),
+                ));
+                for operation_index in 0..OPERATIONS_COUNT {
+                    let mut new_vector =
+                        results[rng.generate_range(0..operation_index + 1)].clone();
+                    new_vector.insert(
+                        rng.generate_range(0..new_vector.len()),
+                        Arc::new(rng.generate_range(0..usize::MAX)),
+                    );
+                    results.push(new_vector);
+                }
+            })
+        });
+        bencher_context.bench_function("insert vector rpds", |b| {
+            b.iter(|| {
+                let mut rng = WyRand::new_seed(SEED);
+                let mut results: Vec<rpds::VectorSync<usize>> =
+                    Vec::with_capacity(OPERATIONS_COUNT);
+                results.push(rpds::VectorSync::from_iter(
+                    (0..VECTOR_SIZE).map(|_| rng.generate_range(0..usize::MAX)),
+                ));
+                for operation_index in 0..OPERATIONS_COUNT {
+                    let mut new_vector =
+                        results[rng.generate_range(0..operation_index + 1)].clone();
+                    let insertion_index = rng.generate_range(0..new_vector.len());
+                    new_vector.push_back_mut(*new_vector.last().unwrap());
+                    for index_to_shift_previous_value_to in
+                        (insertion_index + 1)..(new_vector.len() - 2)
+                    {
+                        new_vector.set_mut(
+                            index_to_shift_previous_value_to,
+                            new_vector[index_to_shift_previous_value_to - 1],
+                        );
+                    }
+                    new_vector[insertion_index] = rng.generate_range(0..usize::MAX);
+                    results.push(new_vector);
+                }
+            })
+        });
     }
 }
 
